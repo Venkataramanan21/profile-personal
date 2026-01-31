d7a1ec62c00fc48ab5798fea6ef5b21f279802ff
Venkataramanan B <venkatbala214@gmail.com>
2026-01-31 16:48:21 +0530

    React Icons addition

--- src/components/shared/HeroTeaser/HeroTeaser.tsx
+++ src/components/shared/HeroTeaser/HeroTeaser.tsx
@@ -1,5 +1,7 @@
 import PaintPath from '../PaintPath/PaintPath';
 import Styles from './HeroTeaser.module.css';
+import { FaCode, FaDownload } from 'react-icons/fa';
+
 const HeroTeaser = () => {
   return (
     <div className={Styles.container}>
@@ -8,8 +10,13 @@ const HeroTeaser = () => {
       </div>
       <div className={Styles.header}>
         <div className={Styles.logoContainer}>
-          <img className={Styles.logo} src="zesova.jpg" />
-        </div>
+          {/* I need to give a hover effect that makes it feel lie it grows */}
+
+          
+          <div className="p-2 transition-[padding] duration-500 ease-out hover:p-4">
+              <img className={Styles.logo} src="zesova.jpg" />
+            </div>
+          </div>
         <div>Hi I am,</div>
 
         <div
@@ -17,7 +24,18 @@ const HeroTeaser = () => {
         >
           Venkataramanan b
         </div>
-        <div>Front End Developer - My Source Code needs caffeine to spark!</div>
+        <div>Full-Stack Developer | Java, React & SQL Expert | My Source Code needs caffeine to spark!</div>
+
+        <div className="flex gap-4 mt-6 flex-wrap justify-center">
+          <button className="flex items-center px-6 py-3 bg-teal-600 text-white font-semibold rounded-full hover:border-none shadow-md hover:bg-teal-700 transition duration-300 ease-in-out transform hover:scale-105">
+            <FaCode className="mr-2" />
+            View My Work
+          </button>
+          <button className="flex items-center px-6 py-3 bg-transparent border-2 border-teal-600 text-teal-600 font-semibold rounded-full hover:border-teal-700 hover:bg-green-100 transition duration-300 ease-in-out transform hover:scale-105">
+            <FaDownload className="mr-2" />
+            Download My Resume
+          </button>
+        </div>
       </div>
     </div>
   );
